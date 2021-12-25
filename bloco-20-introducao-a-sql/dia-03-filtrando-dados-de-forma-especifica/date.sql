5bd241415eb79e3892b519cd136eec321baeb07d
Juan Formoso <trajano.juan@gmail.com>
2021-12-25 20:09:23 -0300

    Add date exercises

---
+++ bloco-20-introducao-a-sql/dia-03-filtrando-dados-de-forma-especifica/date.sql
@@ -0,0 +1,18 @@
+SELECT COUNT(*) Pagamentos FROM sakila.payment
+WHERE DATE(payment_date) = '2005-05-25';
+
+SELECT COUNT(*) Pagamentos FROM sakila.payment
+WHERE DATE(payment_date) BETWEEN '2005-07-01' AND '2005-08-22';
+
+SELECT DATE(rental_date) AS Data,
+YEAR(rental_date) AS Ano ,
+MONTH(rental_date) AS Mes,
+DAY(rental_date) AS Dia,
+HOUR(rental_date) AS Hora,
+MINUTE(rental_date) AS Minuto,
+SECOND(rental_date) AS Segundo
+FROM sakila.rental
+WHERE rental_id = 10330;
+
+SELECT * FROM sakila.payment
+WHERE DATE(payment_date) = '2005-07-28' AND HOUR(payment_date) >= 22;
