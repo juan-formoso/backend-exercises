24ab0328bb3ad1b9939a1bf9594b377c2d261834
Juan Formoso <trajano.juan@gmail.com>
2021-12-25 19:53:54 -0300

    Add in and between exercises

---
+++ bloco-20-introducao-a-sql/dia-03-filtrando-dados-de-forma-especifica/in-between.sql
@@ -0,0 +1,19 @@
+SELECT first_name, last_name, email FROM sakila.customer
+WHERE last_name IN('hicks', 'crawford', 'henry', 'boyd', 'mason', 'morales', 'kennedy')
+ORDER BY first_name;
+
+SELECT email FROM sakila.customer
+WHERE address_id BETWEEN 172 AND 176
+ORDER BY email;
+
+SELECT COUNT(*) quantidade_de_pagamentos FROM sakila.payment
+WHERE DATE(payment_date) BETWEEN '2005-05-01' AND '2005-08-01';
+
+SELECT title, release_year, rental_duration FROM sakila.film
+WHERE rental_duration BETWEEN 3 AND 6
+ORDER BY rental_duration DESC, title;
+
+SELECT title, rating FROM sakila.film
+WHERE rating IN ('G', 'PG', 'PG-13')
+ORDER BY title 
+LIMIT 500;
