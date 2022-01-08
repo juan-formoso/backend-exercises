fbb023565874b7590ed2ad74b560b9132c46d142
Juan Formoso <trajano.juan@gmail.com>
2022-01-08 16:21:26 -0300

    Complete exercise 5

--- bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/imc.js
+++ bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -11,6 +11,22 @@ function calculaImc() {
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
 
   console.log(`IMC: ${imc}`);
+
+  if (imc < 18.5) {
+    console.log("Abaixo do peso");
+  }
+  if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Peso normal");
+  }
+  if (imc >= 25 && imc <= 29.9) {
+    console.log("Obesidade grau I");
+  }
+  if (imc >= 30 && imc <= 39.9) {
+    console.log("Obesidade grau II");
+  }
+  if (imc >= 40) {
+    console.log("Obesidade grau III e IV");
+  }
 }
 
 calculaImc();
