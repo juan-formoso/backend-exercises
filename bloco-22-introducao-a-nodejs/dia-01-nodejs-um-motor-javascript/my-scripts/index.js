defb2fd70f3c5535eb4b1df589ef1d878bf37475
Juan Formoso <trajano.juan@gmail.com>
2022-01-08 17:23:27 -0300

    Add bonus scripts

--- bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/index.js
+++ bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/index.js
@@ -4,6 +4,8 @@ const scripts = [
   { name: "Calcular IMC", script: "./imc.js" },
   { name: "Calcular velocidade média", script: "./velocidade.js" },
   { name: "Jogo de adivinhação", script: "./sorteio.js" },
+  { name: "Calcular fatorial", script: "./fatorial.js" },
+  { name: "Exibir n números de Fibonacci", script: "./fibonacci.js" },
 ];
 
 let messages = scripts.map((script, index) => `${index + 1} - ${script.name}`);
