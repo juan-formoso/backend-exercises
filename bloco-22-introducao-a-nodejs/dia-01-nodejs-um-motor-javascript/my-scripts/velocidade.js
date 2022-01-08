d6bcc72c18166e2db4e58c310afe14fdae3dc4bc
Juan Formoso <trajano.juan@gmail.com>
2022-01-08 16:45:14 -0300

    Add questionInt

--- bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/velocidade.js
+++ bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/velocidade.js
@@ -1,6 +1,6 @@
 const readline = require("readline-sync");
-const distancia = readline.question("Distância (m): ");
-const tempo = readline.question("Tempo gasto na viagem (s): ");
+const distancia = readline.questionInt("Distância (m): ");
+const tempo = readline.questionInt("Tempo gasto na viagem (s): ");
 
 const velocidadeMedia = () => {
   const velocidade = (distancia / tempo).toFixed(2);
