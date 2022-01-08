9afeee4661077dfcab11c1ebdb8072e896752f0d
Juan Formoso <trajano.juan@gmail.com>
2022-01-08 02:18:40 -0300

    Add hello world in node js

---
+++ bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/hello-world/index.js
@@ -0,0 +1,5 @@
+const readline = require("readline-sync");
+const name = readline.question("What is your name? ");
+const age = readline.questionInt("How old are you? ");
+
+console.log(`Hello ${name}! You are ${age} years old.`);
