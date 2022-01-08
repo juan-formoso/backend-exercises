53059242295186f5b2dcc97033ee98e3087d1d97
Juan Formoso <trajano.juan@gmail.com>
2022-01-08 17:18:49 -0300

    Add exercise 8

---
+++ bloco-22-introducao-a-nodejs/dia-01-nodejs-um-motor-javascript/my-scripts/index.js
@@ -0,0 +1,19 @@
+const readline = require("readline-sync");
+
+const scripts = [
+  { name: "Calcular IMC", script: "./imc.js" },
+  { name: "Calcular velocidade média", script: "./velocidade.js" },
+  { name: "Jogo de adivinhação", script: "./sorteio.js" },
+];
+
+let messages = scripts.map((script, index) => `${index + 1} - ${script.name}`);
+
+messages.unshift("Escolha um númeropara executar o script correspondente");
+messages = messages.join("\n");
+
+const scriptNumber = readline.questionInt(messages) - 1;
+const script = scripts[scriptNumber];
+
+if (!script) console.log("Script inválido");
+
+require(script.script);
