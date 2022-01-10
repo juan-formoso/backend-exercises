eeeb11b8a4f21baebc4f46e81251e9542226fec8
Juan Formoso <trajano.juan@gmail.com>
2022-01-10 15:38:08 -0300

    Add promise method in read file

--- bloco-22-introducao-a-nodejs/dia-02-fluxo-assincrono/io-local/readFile.js
+++ bloco-22-introducao-a-nodejs/dia-02-fluxo-assincrono/io-local/readFile.js
@@ -1,10 +1,11 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const fileName = "meu-arquivo.txt";
 
-fs.readFile(fileName, "utf-8", (err, data) => {
-  if (err) {
-    console.error(`Não foi possível ler o arquivo ${fileName}\n Erro: ${err}`);
+fs.readFile(fileName, "utf-8")
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo: ${fileName}\n Erro: ${err}`);
     process.exit(1);
-  }
-  console.log(`Conteúdo do arquivo: ${data}`);
-});
+  });
