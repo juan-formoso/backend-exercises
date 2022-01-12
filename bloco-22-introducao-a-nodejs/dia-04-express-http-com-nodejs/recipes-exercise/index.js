d7cd35d4696e5505a2c8ccc2310a48a6e7680109
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 19:42:01 -0300

    Add query string exercise

--- bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/recipes-exercise/index.js
+++ bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/recipes-exercise/index.js
@@ -22,6 +22,17 @@ app.get("/recipes", function (req, res) {
   res.json(recipes.sort());
 });
 
+app.get("/recipes/search", function (req, res) {
+  const { name, maxPrice, minPrice } = req.query;
+  const filteredRecipes = recipes.filter(
+    (r) =>
+      r.name.includes(name) &&
+      r.price <= parseInt(maxPrice) &&
+      r.price >= parseInt(minPrice)
+  );
+  res.status(200).json(filteredRecipes);
+});
+
 app.get("/recipes/:id", function (req, res) {
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === parseInt(id));
@@ -33,6 +44,12 @@ app.get("/recipes/:id", function (req, res) {
 
 app.get("/drinks", function (req, res) {
   res.json(drinks.sort());
+});
+
+app.get("/drinks/search", function (req, res) {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((d) => d.name.includes(name));
+  res.status(200).json(filteredDrinks);
 });
 
 app.get("/drinks/:id", function (req, res) {
