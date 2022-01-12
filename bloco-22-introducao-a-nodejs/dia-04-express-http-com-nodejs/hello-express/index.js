efe2aa48ba7d60efc137a425b185f46a24aabb6c
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 16:03:32 -0300

    Corrigindo função

--- bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/hello-express/index.js
+++ bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/hello-express/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", handleHelloWorldRequest);
+
 app.listen(3001, () => {
   console.log("Aplicação ouvindo na porta 3001");
 });
-const handleHelloWorldRequest = (request, response) => {
+
+function handleHelloWorldRequest(request, response) {
   response.status(200).send("Hello World!");
-};
+}
