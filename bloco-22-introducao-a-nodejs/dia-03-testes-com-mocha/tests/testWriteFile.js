076044feff87d58d625f84f31175371583e602a0
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 01:14:00 -0300

    Add test with sinon

--- bloco-22-introducao-a-nodejs/dia-03-testes-com-mocha/tests/testWriteFile.js
+++ bloco-22-introducao-a-nodejs/dia-03-testes-com-mocha/tests/testWriteFile.js
@@ -1,8 +1,15 @@
 const fs = require("fs");
+const sinon = require("sinon");
 const { expect } = require("chai");
 const writeFile = require("../src/writeFile");
 
 describe("Executa writeFile", () => {
+  before(() => {
+    sinon.stub(fs, "writeFileSync");
+  });
+  after(() => {
+    fs.writeFileSync.restore();
+  });
   describe("se a resposta for", () => {
     it("uma string", () => {
       const parameters = writeFile("file.txt", "Aatrox");
