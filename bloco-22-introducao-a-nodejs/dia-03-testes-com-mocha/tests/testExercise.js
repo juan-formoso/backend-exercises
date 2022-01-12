0901f7e738c57a61856dcb9ce847531663a936b5
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 00:31:07 -0300

    Correct const name

--- bloco-22-introducao-a-nodejs/dia-03-testes-com-mocha/tests/testExercise.js
+++ bloco-22-introducao-a-nodejs/dia-03-testes-com-mocha/tests/testExercise.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 
-const file = require("../exercise");
+const validateNumbers = require("../exercise.js");
 
 describe("Executa a função validateNumbers", () => {
   describe("se o número passado for maior que 0", () => {
