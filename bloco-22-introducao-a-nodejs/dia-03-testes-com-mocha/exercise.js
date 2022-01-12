aca5fc2ce090af731a72a61d229b6b73392c19bd
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 00:55:37 -0300

    Refatorando codigo

--- bloco-22-introducao-a-nodejs/dia-03-testes-com-mocha/exercise.js
+++ bloco-22-introducao-a-nodejs/dia-03-testes-com-mocha/exercise.js
@@ -1,9 +1,6 @@
 module.exports = (number) => {
-  if (number > 0) {
-    return "positivo";
-  } else if (number < 0) {
-    return "negativo";
-  } else {
-    return "neutro";
-  }
+  if (typeof number !== "number") return "o valor deve ser um número";
+  if (number > 0) return "positivo";
+  if (number < 0) return "negativo";
+  return "neutro";
 };
