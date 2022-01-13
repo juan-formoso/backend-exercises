73530e53d87e0e66d9de98b4fa5f0c6984a19eb3
Juan Formoso <trajano.juan@gmail.com>
2022-01-13 15:29:17 -0300

    Add found user function

--- bloco-22-introducao-a-nodejs/dia-05-middlewares/recipes/middleware/auth-middleware.js
+++ bloco-22-introducao-a-nodejs/dia-05-middlewares/recipes/middleware/auth-middleware.js
@@ -1,7 +1,9 @@
-const validUser = {
-  username: "Patolino",
-  password: "12345678",
-};
+const validUser = [
+  { username: "Patolino", password: "12345678" },
+  { username: "Pernalonga", password: "abcd1234" },
+  { username: "Papaleguas", password: "abcd1234" },
+  { username: "Frajola", password: "abcd1234" },
+];
 
 const authMiddleware = (req, res, next) => {
   const { username, password } = req.headers;
@@ -10,11 +12,14 @@ const authMiddleware = (req, res, next) => {
       .status(401)
       .json({ message: "Username or password can not be blank!" });
   }
+  const foundUser = validUser.find((user) => user.username === username);
+  if (!foundUser) return res.status(401).json({ message: "Invalid username!" });
   if (username !== validUser.username || password !== validUser.password) {
     return res
       .status(401)
       .json({ message: "Username or password is incorrect!" });
   }
+  req.user = foundUser;
   next();
 };
 
