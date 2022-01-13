3a452b317bc632815a6c77fefb54e81149852706
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 23:06:43 -0300

    Add fetch api

--- bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/recipes-exercise/index.js
+++ bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/recipes-exercise/index.js
@@ -110,7 +110,7 @@ app.listen(3001, () => {
 });
 
 /* FETCH DO METODO 'POST' */
-/* fetch(`http://localhost:3001/recipes/`, {
+fetch(`http://localhost:3001/recipes/`, {
   method: "POST",
   headers: {
     Accept: "application/json",
@@ -121,4 +121,26 @@ app.listen(3001, () => {
     name: "Macarrão com Frango",
     price: 30,
   }),
-}); */
+});
+
+/* Requisição do tipo PUT */
+fetch(`http://localhost:3001/recipes/2`, {
+  method: "PUT",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "Macarrão ao alho e óleo",
+    price: 40,
+  }),
+});
+
+/* Requisição do tipo DELETE */
+fetch(`http://localhost:3001/recipes/4`, {
+  method: "DELETE",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
