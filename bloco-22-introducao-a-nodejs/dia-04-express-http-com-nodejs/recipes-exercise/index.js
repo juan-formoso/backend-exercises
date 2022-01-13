99a483224b85a99b750d99d4af07462155d0dcc5
Juan Formoso <trajano.juan@gmail.com>
2022-01-12 23:02:28 -0300

    Add update and remove functions

--- bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/recipes-exercise/index.js
+++ bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/recipes-exercise/index.js
@@ -59,10 +59,29 @@ app.get("/recipes/search", function (req, res) {
 app.get("/recipes/:id", function (req, res) {
   const { id } = req.params;
   const recipe = recipes.find((r) => r.id === parseInt(id));
-
   if (!recipe) return res.status(404).json({ message: "Recipe not found!" });
-
   res.status(200).json(recipe);
+});
+
+/* PUT (UPDATE) METHOD RECIPES */
+app.put("/recipes/:id", function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+  if (recipeIndex === -1)
+    return res.status(404).json({ message: "Recipe not found!" });
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+  res.status(204).end();
+});
+
+/* DELETE (REMOVE) METHOD RECIPES */
+app.delete("/recipes/:id", function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+  if (recipeIndex === -1)
+    return res.status(404).json({ message: "Recipe not found!" });
+  recipes.splice(recipeIndex, 1);
+  res.status(204).end();
 });
 
 /* REQUISIÇÃO VIA 'GET' DAS BEBIDAS */
