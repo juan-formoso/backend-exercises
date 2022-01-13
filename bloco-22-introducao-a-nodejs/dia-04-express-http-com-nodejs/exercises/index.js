a980ee72eae98c03f9973c4d83ae9d13f97c0273
Juan Formoso <trajano.juan@gmail.com>
2022-01-13 01:26:05 -0300

    Add get and post endpoints to return and add info

--- bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/exercises/index.js
+++ bloco-22-introducao-a-nodejs/dia-04-express-http-com-nodejs/exercises/index.js
@@ -1,17 +1,56 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const rescue = require("express-rescue");
 const app = express();
+const simpsonsUtils = require("./fs-utils");
 
 app.use(bodyParser.json());
 
 /* GET ROUTE */
 app.get("/ping", (_req, res) => res.json({ message: "pong" }));
 
+// Endpoint to Simpsons
+app.get(
+  "/simpsons",
+  rescue(async (req, res) => {
+    const simpsons = await simpsonsUtils.getSimpsons();
+    res.status(200).json(simpsons);
+  })
+);
+
+// Endpoint to Simpsons by ID
+app.get(
+  "/simpsons/:id",
+  rescue(async (req, res) => {
+    const simpsons = await simpsonsUtils.getSimpsons();
+    const simpson = simpsons.find(({ id }) => id === req.params.id);
+    if (!simpson) {
+      return res.status(404).json({ message: "simpson not found" });
+    }
+    return res.status(202).json(simpson);
+  })
+);
+
 /* POST ROUTE */
 app.post("/hello", (req, res) => {
   const { name } = req.body;
   res.status(200).json({ message: `Hello, ${name}!` });
 });
+
+// Endpoint to add a new simpson
+app.post(
+  "/simpsons",
+  rescue(async (req, res) => {
+    const { id, name } = req.body;
+    const simpsons = await simpsonsUtils.getSimpsons();
+    if (simpsons.map(({ id }) => id).includes(id)) {
+      return res.status(409).json({ message: "id already exists" });
+    }
+    simpsons.push({ id, name });
+    await simpsonsUtils.setSimpsons(simpsons);
+    res.status(204).end();
+  })
+);
 
 /* PUT ROUTE */
 app.put("/users/:name/:age", (req, res) => {
