c2643afebe17001e970e87c8dcdecb558316ab26
Juan Formoso <trajano.juan@gmail.com>
2022-01-17 22:35:23 -0300

    Add having exercises

--- bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/dados-agrupados/data3.sql
+++ bloco-21-funcoes-sql-joins-e-normalizacao/dia-01-funcoes-mais-usadas-no-sql/dados-agrupados/data3.sql
@@ -1,3 +1,20 @@
 SELECT district, COUNT(*) FROM sakila.address
 GROUP BY district
 HAVING COUNT(*) > 5;
+
+SELECT first_name, COUNT(*)
+FROM sakila.actor
+GROUP BY first_name
+HAVING COUNT(*) > 2;
+
+SELECT first_name, COUNT(*) AS nomes_cadastrados
+FROM sakila.actor
+GROUP BY first_name
+HAVING nomes_cadastrados > 2;
+
+/*ISSO AQUI NÃO FUNCIONA:
+SELECT first_name, COUNT(*) AS 'nomes cadastrados'
+FROM sakila.actor
+GROUP BY first_name
+HAVING 'nomes cadastrados' > 2;
+*/
