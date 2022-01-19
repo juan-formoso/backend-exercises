05eb623ce97352ad60228fe8cf6be2f86c916ba9
Juan Formoso <trajano.juan@gmail.com>
2022-01-19 19:06:13 -0300

    Add função que gera uma string contendo o fullName da pessoa

--- bloco-23-nodejs-e-arquitetura-de-software/dia-01-camada-de-model/model-com-mysql/models/Author.js
+++ bloco-23-nodejs-e-arquitetura-de-software/dia-01-camada-de-model/model-com-mysql/models/Author.js
@@ -1,5 +1,21 @@
 const connection = require("./connection");
 
+// Cria uma string com o nome completo do autor
+const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
+  // Note que `Boolean` é uma função que recebe um parâmetro e retorna true ou false
+  // nesse caso, se middle_name for `undefined` ou uma string vazia o retorno será `false`
+  const fullName = [first_name, middle_name, last_name]
+    .filter(Boolean)
+    .join(" ");
+  return {
+    id,
+    firstName,
+    middleName,
+    lastName,
+    fullName,
+  };
+};
+
 // Converte o nome dos campos de snake_case para camelCase
 const serialize = (authorData) => ({
   id: authorData.id,
@@ -12,7 +28,7 @@ const getAll = async () => {
   const [authors] = await connection.execute(
     "SELECT id, first_name, middle_name, last_name FROM model_example.authors;"
   );
-  return authors.map(serialize);
+  return authors.map(serialize).map(getNewAuthor);
 };
 
 module.exports = {
