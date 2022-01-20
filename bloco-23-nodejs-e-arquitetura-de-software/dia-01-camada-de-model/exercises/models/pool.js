e2d0faa40c0b29ed0a9c3dde8694d1ba88e5ff7d
Juan Formoso <trajano.juan@gmail.com>
2022-01-20 11:26:46 -0300

    Add pool.js

---
+++ bloco-23-nodejs-e-arquitetura-de-software/dia-01-camada-de-model/exercises/models/pool.js
@@ -0,0 +1,13 @@
+// Importação do driver do banco
+const mysql = require("mysql2/promise");
+
+// Criando uma "pool" de conexões com o banco de dados
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "minhasenha",
+  database: "users_crud",
+});
+
+// Retornando a pool criada para ser utilizada pelo model
+module.exports = pool;
