54395b885899c1fe1585be7719631725a2628c6a
Juan Formoso <trajano.juan@gmail.com>
2022-02-01 16:14:44 -0300

    Add Book model

---
+++ bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-database/exercises/models/Book.js
@@ -0,0 +1,23 @@
+const Book = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pageQuantity: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    }
+  });
+  return Book;
+};
+
+module.exports = Book;
