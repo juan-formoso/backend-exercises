a6ab523f80d79b7d0ff2f0f50149fdb69948f922
Juan Formoso <trajano.juan@gmail.com>
2022-02-01 15:53:56 -0300

    Add books table up and down script

--- bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-database/exercises/migrations/20220201182853-create-books.js
+++ bloco-24-nodejs-orm-e-autenticacao/dia-01-orm-interface-da-aplicacao-com-database/exercises/migrations/20220201182853-create-books.js
@@ -1,21 +1,32 @@
-'use strict';
-
+"use strict";
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Books", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTENGER,
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Books");
+  },
 };
