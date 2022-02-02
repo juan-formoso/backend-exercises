71561f3aca87f09e1328f47be447d864f030b05f
Juan Formoso <trajano.juan@gmail.com>
2022-02-02 16:13:26 -0300

    Update Hash.js

--- bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/hash.js
+++ bloco-24-nodejs-orm-e-autenticacao/dia-03-json-web-token/hash.js
@@ -18,3 +18,10 @@ const payload = JSON.stringify({
 const secret = "MyPassword123";
 const assinatura = hmac(`${base64(header)}.${base64(payload)}`, secret);
 const token = `${base64(header)}.${base64(payload)}.${base64(assinatura)}`;
+
+// Exemplo de envio de um JWT via header em uma requisição HTTP
+/* 
+GET /foo/bar HTTP/1.1
+Host: www.exemplo.com
+Authorization: Bearer (Header em base64).(Payload em base64).(Assinatura em base64)
+*/
