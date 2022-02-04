5ee017d89d0259e691f00ba10d3aca4b02e7fa32
Juan Formoso <trajano.juan@gmail.com>
2022-02-04 15:52:20 -0300

    Update model user with phone_num

--- bloco-24-nodejs-orm-e-autenticacao/monitoria-sequelize/models/user.js
+++ bloco-24-nodejs-orm-e-autenticacao/monitoria-sequelize/models/user.js
@@ -1,9 +1,9 @@
 const User = (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     fullName: DataTypes.STRING,
-  }, {
-    underscored: true
+    phone_num: DataTypes.STRING,
   });
+
   return User;
 };
 
