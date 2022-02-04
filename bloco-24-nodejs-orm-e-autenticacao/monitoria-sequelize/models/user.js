5d342dada61528a8a2a55a1b63faf9f1d6161483
Juan Formoso <trajano.juan@gmail.com>
2022-02-04 13:32:17 -0300

    Muda classe user para arrow function

--- bloco-24-nodejs-orm-e-autenticacao/monitoria-sequelize/models/user.js
+++ bloco-24-nodejs-orm-e-autenticacao/monitoria-sequelize/models/user.js
@@ -1,23 +1,11 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  User.init({
-    fullName: DataTypes.STRING
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
   }, {
-    sequelize,
-    modelName: 'User',
+    underscored: true,
   });
   return User;
 };
+
+module.exports = User;
