eafe2e8bf3af024cd3eb182571cac269a9c943b5
Juan Formoso <trajano.juan@gmail.com>
2022-02-17 18:42:58 -0300

    Add inference.ts

---
+++ bloco-26-typescript/dia-01-introducap-ao-typescript/tipos-e-subtipos/inference.ts
@@ -0,0 +1,3 @@
+let flag = true; // o compilador irá inferir o tipo boolean
+const numberPI = 3.1416; // o compilador irá inferir o tipo number
+let message = "Hello World!"; // o compilador irá inferir o tipo string
