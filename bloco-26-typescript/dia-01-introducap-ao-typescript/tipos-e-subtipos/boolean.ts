ec8d234cc9b0cf7c643dae6c99fbebc58abe8a96
Juan Formoso <trajano.juan@gmail.com>
2022-02-17 18:42:48 -0300

    Add boolean.ts

---
+++ bloco-26-typescript/dia-01-introducap-ao-typescript/tipos-e-subtipos/boolean.ts
@@ -0,0 +1,3 @@
+// Boolean recebe verdadeiro (true) ou falso (false)
+let yes: boolean = true; // cria uma variável de nome "yes" e diz que o tipo é booleano e o valor é true
+let no: boolean = false; // cria uma variável de nome "no" e diz que o tipo é booleano e o valor é false
