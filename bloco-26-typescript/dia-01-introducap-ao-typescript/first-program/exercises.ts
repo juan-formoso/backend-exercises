c4b1ed5dbcc99d4a312e4d79a768214146f34fdf
Juan Formoso <trajano.juan@gmail.com>
2022-02-17 19:16:02 -0300

    Update exercises.ts's functions

--- bloco-26-typescript/dia-01-introducap-ao-typescript/first-program/exercises.ts
+++ bloco-26-typescript/dia-01-introducap-ao-typescript/first-program/exercises.ts
@@ -1,3 +1,15 @@
 export const greeter = (name: string) => {
   return `Hello ${name}!`;
 };
+
+export const personAge = (name: string, age: number) => {
+  return `${name} is ${age} years old!`;
+};
+
+export const add = (a: number, b: number) => {
+  return a + b;
+};
+
+export const sumArray = (numbers: number[]) => {
+  return numbers.reduce(add, 0);
+};
