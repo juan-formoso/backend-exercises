64268cb40c5efb7b6bf219dd9cefbc6beb478265
Juan Formoso <trajano.juan@gmail.com>
2022-02-17 18:43:13 -0300

    Add numbers.ts

---
+++ bloco-26-typescript/dia-01-introducap-ao-typescript/tipos-e-subtipos/number.ts
@@ -0,0 +1,8 @@
+// Number recebe valores numéricos e assim como no JavaScript todos os valores são de ponto flutuante.
+
+// cria uma variável de nome "x" e diz que o tipo é number mas não seta o valor
+// isso não funciona com const
+let x: number;
+
+let y: number = 0;
+let z: number = 123.456;
