8149d3e76f345c07d82429bf41268aac80be9668
Juan Formoso <trajano.juan@gmail.com>
2022-02-17 19:24:12 -0300

    Update functions's type + add rectangle, square a

--- bloco-26-typescript/dia-01-introducap-ao-typescript/first-program/exercises.ts
+++ bloco-26-typescript/dia-01-introducap-ao-typescript/first-program/exercises.ts
@@ -6,14 +6,22 @@ export const personAge = (name: string, age: number) => {
   return `${name} is ${age} years old!`;
 };
 
-export const add = (a: number, b: number) => {
+export const add = (a: number, b: number): number => {
   return a + b;
 };
 
-export const sumArray = (numbers: number[]) => {
+export const sumArray = (numbers: number[]): number => {
   return numbers.reduce(add, 0);
 };
 
-export const triangleArea = (base: number, height: number) => {
+export const triangleArea = (base: number, height: number): number => {
   return (base * height) / 2;
+};
+
+export const squareArea = (side: number): number => {
+  return side ** 2;
+};
+
+export const rectangleArea = (base: number, height: number): number => {
+  return base * height;
 };
