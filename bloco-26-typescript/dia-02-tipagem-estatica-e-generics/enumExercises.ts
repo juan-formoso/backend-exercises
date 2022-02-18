6a2899b63be1232c2d855c3b737d780e273a7bfe
Juan Formoso <trajano.juan@gmail.com>
2022-02-18 19:13:15 -0300

    add more exercises

---
+++ bloco-26-typescript/dia-02-tipagem-estatica-e-generics/enumExercises.ts
@@ -0,0 +1,38 @@
+// Crie um Enum que represente os dias da semana. Seu valor deve ser número do dia.
+enum weekDays {
+  Sunday = 1,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+// Crie um enum que represente as cores do arco-íris. Seus nomes devem ser em português.
+enum colors {
+  red = "vermelho",
+  blue = "azul",
+  yellow = "amarelo",
+  green = "verde",
+  orange = "laranja",
+  purple = "roxo",
+  indigo = "anil",
+}
+
+// Crie um enum que represente as ações: salvar, imprimir, abrir, visualizar e fechar. Seu valor deve ser do tipo inteiro
+enum actions {
+  save,
+  print,
+  open,
+  view,
+  close,
+}
+
+// Crie um enum que represente os pontos cardeais: norte, sul, leste e oeste. Seu valor deve ser a primeira letra do nome do ponto cardial.
+enum cardinalPoints {
+  North = "N",
+  South = "S",
+  East = "E",
+  West = "W",
+}
