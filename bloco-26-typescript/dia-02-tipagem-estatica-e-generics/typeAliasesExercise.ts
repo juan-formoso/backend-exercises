99ee849c91fbce8b58abc24f86e078f1b9e9d11f
Juan Formoso <trajano.juan@gmail.com>
2022-02-18 20:35:15 -0300

    Add typeAliasesExercise.ts

---
+++ bloco-26-typescript/dia-02-tipagem-estatica-e-generics/typeAliasesExercise.ts
@@ -0,0 +1,18 @@
+// Crie um type para um objeto que represente um pássaro
+type Bird = {
+  name: string;
+  age: number;
+  color: string;
+};
+
+// Crie um type que represente uma função que recebe 2 valores numéricos e retorna a soma dos dois.
+type Sum = (a: number, b: number) => number;
+
+// Crie um type para um objeto que represente um endereço.
+type Address = {
+  street: string;
+  number: number;
+  city: string;
+  state: string;
+  country: string;
+};
