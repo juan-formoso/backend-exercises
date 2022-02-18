a791b6c42899c06c87983cc390042ed914831691
Juan Formoso <trajano.juan@gmail.com>
2022-02-18 20:40:21 -0300

    Add typeUnions exercises

--- bloco-26-typescript/dia-02-tipagem-estatica-e-generics/typeAliasesExercise.ts
+++ bloco-26-typescript/dia-02-tipagem-estatica-e-generics/typeAliasesExercise.ts
@@ -16,3 +16,15 @@ type Address = {
   state: string;
   country: string;
 };
+
+// Crie um type union que represente os estados físicos da matéria: líquido, sólido e gasoso.
+type PhysicalState = "liquid" | "solid" | "gas";
+
+// Crie um type union que represente o documento identificador de uma pessoa que pode receber valores numéricos ou texto. Ex: "123.456.789-10" ou 12345678910.
+type ID = number | string;
+
+// Crie um type union que represente sistemas operacionais: linux, mac os ou windows.
+type OS = "linux" | "mac os" | "windows";
+
+// Crie um type union que represente as vogais do alfabeto.
+type Vowel = "a" | "e" | "i" | "o" | "u";
