02d1e0ce533f2704a86bd7e2b1130b6212321aa8
Juan Formoso <trajano.juan@gmail.com>
2022-02-18 20:37:15 -0300

    Add typeUnion concept

--- bloco-26-typescript/dia-02-tipagem-estatica-e-generics/typeAliases.ts
+++ bloco-26-typescript/dia-02-tipagem-estatica-e-generics/typeAliases.ts
@@ -12,3 +12,10 @@ printCoordinates({ x: 10, y: 20 });
 // saída:
 // O valor da coordenada x é 10
 // O valor da coordenada y é 20
+
+/*TYPE UNIONS*/
+
+// A função abaixo poder receber tanto um númer quanto uma string.
+const returnCPF = (cpf: number | string) => {
+  console.log("Seu CPF é " + cpf);
+};
