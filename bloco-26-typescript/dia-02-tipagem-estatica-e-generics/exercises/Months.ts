0be1b3cffa89bd3485ed6a8a826c691b9aca0d55
Juan Formoso <trajano.juan@gmail.com>
2022-02-19 00:17:13 -0300

    Add Months.ts

---
+++ bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercises/Months.ts
@@ -0,0 +1,17 @@
+// Crie uma enum que represente os meses do ano
+enum Months {
+  JANUARY = "January",
+  FEBRUARY = "February",
+  MARCH = "March",
+  APRIL = "April",
+  MAY = "May",
+  JUNE = "June",
+  JULY = "July",
+  AUGUST = "August",
+  SEPTEMBER = "September",
+  OCTOBER = "October",
+  NOVEMBER = "November",
+  DECEMBER = "December",
+}
+
+export default Months;
