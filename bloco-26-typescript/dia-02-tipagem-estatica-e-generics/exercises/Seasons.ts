7d981ac6881cb07b0875b05435b4e03c61892de5
Juan Formoso <trajano.juan@gmail.com>
2022-02-19 00:18:18 -0300

    Add Seasons.ts

---
+++ bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercises/Seasons.ts
@@ -0,0 +1,9 @@
+// Crie uma enum que represente as estações climáticas do ano.
+enum Seasons {
+  SPRING = "Spring",
+  SUMMER = "Summer",
+  AUTUMN = "Autumn",
+  WINTER = "Winter",
+}
+
+export default Seasons;
