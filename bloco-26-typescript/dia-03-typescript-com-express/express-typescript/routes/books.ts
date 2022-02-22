55d18cffcd17c4bbf808100ee0a41d30a930a05a
Juan Formoso <trajano.juan@gmail.com>
2022-02-22 20:04:47 -0300

    Add get method in books route

--- bloco-26-typescript/dia-03-typescript-com-express/express-typescript/routes/books.ts
+++ bloco-26-typescript/dia-03-typescript-com-express/express-typescript/routes/books.ts
@@ -1,10 +1,25 @@
 import { Router, Request, Response } from "express";
+import { read } from "../functions";
+import StatusCode from "../enums/StatusCode";
 
 const router = Router();
 
-router.get("/books", (req: Request, res: Response) => {});
+const NotFound = "Livro não encontrado.";
 
-router.get("/books/:isbn", (req: Request, res: Response) => {});
+router.get("/books", async (req: Request, res: Response) => {
+  const books = await read();
+  return res.status(StatusCode.OK).json(books);
+});
+
+router.get("/books/:isbn", async (req: Request, res: Response) => {
+  const { isbn } = req.params;
+  const books = await read();
+  const book = books.find((book) => book.isbn === isbn);
+  if (!book) {
+    return res.status(StatusCode.NOT_FOUND).json(NotFound);
+  }
+  return res.status(StatusCode.OK).json(book);
+});
 
 router.post("/books", (req: Request, res: Response) => {});
 
