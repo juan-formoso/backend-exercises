cc824878dc13a7aa23f3d92297c9cb12edb9dcdf
Juan Formoso <trajano.juan@gmail.com>
2022-03-10 21:00:52 -0300

    Add example.ts

---
+++ bloco-27-poo-e-solid/dia-03-polimorfismo/example.ts
@@ -0,0 +1,37 @@
+class Animal {
+  constructor(public name: string) {}
+  move() {
+    console.log(`${this.name} está se movendo.`);
+  }
+}
+
+class Bird extends Animal {
+  move() {
+    console.log(`${this.name} está voando.`);
+  }
+}
+
+class Mammal extends Animal {
+  move() {
+    console.log(`${this.name} está correndo.`);
+  }
+}
+
+const a = new Animal("Animal");
+const b = new Bird("Bird");
+const c = new Mammal("Mammal");
+
+const myMove = (animal: Animal) => {
+  animal.move();
+};
+
+myMove(a);
+myMove(b);
+myMove(c);
+
+/*
+SAÍDA:
+Animal está se movendo.
+Bird está voando.
+Mammal está correndo.
+*/
